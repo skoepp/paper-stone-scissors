a62a9bffae07109e533b503f2ce6c333f09f47ae
Stephan Koepp <skoepp@team.mobile.de>
2013-12-01 13:13:29 +0100

    Update handling player mode.

--- src/resources/js/game.js
+++ src/resources/js/game.js
@@ -7,7 +7,8 @@
  * Copyright 2013 Stephan Koepp. All rights reserved.
  *
  */
-var Game = function () {
+var Game;
+Game = function () {
     'use strict';
 
     var actualGameResultElement, headlineElement, resultElement, resultModel, selectionDisplayElement, selectionElement, playerMode, playerOne, playerSelection, playerTwo, view;
@@ -15,30 +16,20 @@ var Game = function () {
     playerMode = 'Human';
     playerSelection = '';
 
-    function init (actualGameResult, headlineElement, resultElement, selectionElement, selectionDisplayElement) {
-
-        setActualGameResultElement(actualGameResult);
-        setHeadlineElement(headlineElement);
-        setResultElement(resultElement);
-        setSelectionElement(selectionElement);
-        setSelectionDisplayElement(selectionDisplayElement);
-
-        resultModel = new GameResultModel();
-        resultModel.init();
-
-        view = new GameView();
-        view.init(playerMode);
-        view.headlineDisplay(getHeadlineElement());
-        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
-        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
-        view.selectionDisplay(FigureType, getSelectionElement());
-    }
-
+    /*
+     * Define getter and setter for document nodes
+     *
+     * actualGameResultElement
+     * headlineElement
+     * resultElement
+     * selectionElement
+     * selectionDisplayElement
+     */
     function setActualGameResultElement (element) {
         actualGameResultElement = element;
     }
 
-    function getActualGameResultElement (element) {
+    function getActualGameResultElement () {
         return actualGameResultElement;
     }
 
@@ -70,10 +61,13 @@ var Game = function () {
         selectionDisplayElement = element;
     }
 
-    function getSelectionDisplayElement (element) {
+    function getSelectionDisplayElement () {
         return selectionDisplayElement;
     }
 
+    /*
+     * Getter and setter for player mode
+     */
     function getPlayerMode () {
         return playerMode;
     }
@@ -85,16 +79,9 @@ var Game = function () {
         view.headlineDisplay(getHeadlineElement());
     }
 
-    function resetResults () {
-        if (getPlayerMode() === 'Human') {
-            setPlayerSelection('');
-        }
-        resultModel.resetPlayerResults();
-        view.actualGameResultDisplay(getActualGameResultElement(), '');
-        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
-        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
-    }
-
+    /*
+     * Getter and setter for player selection
+     */
     function setPlayerSelection (selectKey) {
         playerSelection = selectKey;
         view.actualGameResultDisplay(getActualGameResultElement(), '');
@@ -105,15 +92,9 @@ var Game = function () {
         return playerSelection;
     }
 
-    function checkSelectionValue () {
-        if (getPlayerSelection() === '') {
-            view.displayNoSelection();
-            return true;
-        }
-
-        return false;
-    }
-
+    /*
+     * Method to get figure object by ramdom selection
+     */
     function computerSelectFigure () {
 
         var zz;
@@ -131,10 +112,34 @@ var Game = function () {
         return null;
     }
 
+    /*
+     * Method to get figure object by selection of human player
+     */
     function personSelectFigure () {
         return FigureFactory.createFigure(playerSelection);
     }
 
+    /*
+     * Helper method check if an item is selected
+     *
+     * shows dialog when no item is selected
+     */
+    function checkSelectionValue () {
+        if (getPlayerSelection() === '') {
+            view.displayNoSelection();
+            return true;
+        }
+
+        return false;
+    }
+
+    /*
+     * Handler for playing a game
+     *
+     * selects figures for players and determines result
+     *
+     * updates view elements
+     */
     function play () {
         var result;
 
@@ -158,11 +163,52 @@ var Game = function () {
         view.showSelectedDisplay(getSelectionDisplayElement(), playerOne.getName(), playerTwo.getName());
     }
 
+    /*
+     * Handler for reset of result
+     */
+    function resetResults () {
+        if (getPlayerMode() === 'Human') {
+            setPlayerSelection('');
+        }
+        resultModel.resetPlayerResults();
+        view.actualGameResultDisplay(getActualGameResultElement(), '');
+        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
+        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
+    }
+
+    /*
+     * init method for game
+     *
+     * 1. set node elements for display
+     * 2. creates and initializes game result model
+     * 3. creates game view object and setup view
+     */
+    function init (actualGameResult, headlineElement, resultElement, selectionElement, selectionDisplayElement) {
+
+        setActualGameResultElement(actualGameResult);
+        setHeadlineElement(headlineElement);
+        setResultElement(resultElement);
+        setSelectionElement(selectionElement);
+        setSelectionDisplayElement(selectionDisplayElement);
+
+        resultModel = new GameResultModel();
+        resultModel.init();
+
+        view = new GameView();
+        view.init(playerMode);
+        view.headlineDisplay(getHeadlineElement());
+        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
+        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
+        view.selectionDisplay(FigureType, getSelectionElement());
+    }
+
     return {
         init: init,
         play: play,
         resetResults: resetResults,
+        getPlayerMode: getPlayerMode,
         setPlayerMode: setPlayerMode,
+        getPlayerSelection: getPlayerSelection,
         setPlayerSelection: setPlayerSelection
     };
 };
--- src/resources/js/puzzle.concat.js
+++ src/resources/js/puzzle.concat.js
@@ -849,7 +849,8 @@ var GameView = function () {
  * Copyright 2013 Stephan Koepp. All rights reserved.
  *
  */
-var Game = function () {
+var Game;
+Game = function () {
     'use strict';
 
     var actualGameResultElement, headlineElement, resultElement, resultModel, selectionDisplayElement, selectionElement, playerMode, playerOne, playerSelection, playerTwo, view;
@@ -857,30 +858,20 @@ var Game = function () {
     playerMode = 'Human';
     playerSelection = '';
 
-    function init (actualGameResult, headlineElement, resultElement, selectionElement, selectionDisplayElement) {
-
-        setActualGameResultElement(actualGameResult);
-        setHeadlineElement(headlineElement);
-        setResultElement(resultElement);
-        setSelectionElement(selectionElement);
-        setSelectionDisplayElement(selectionDisplayElement);
-
-        resultModel = new GameResultModel();
-        resultModel.init();
-
-        view = new GameView();
-        view.init(playerMode);
-        view.headlineDisplay(getHeadlineElement());
-        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
-        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
-        view.selectionDisplay(FigureType, getSelectionElement());
-    }
-
+    /*
+     * Define getter and setter for document nodes
+     *
+     * actualGameResultElement
+     * headlineElement
+     * resultElement
+     * selectionElement
+     * selectionDisplayElement
+     */
     function setActualGameResultElement (element) {
         actualGameResultElement = element;
     }
 
-    function getActualGameResultElement (element) {
+    function getActualGameResultElement () {
         return actualGameResultElement;
     }
 
@@ -912,10 +903,13 @@ var Game = function () {
         selectionDisplayElement = element;
     }
 
-    function getSelectionDisplayElement (element) {
+    function getSelectionDisplayElement () {
         return selectionDisplayElement;
     }
 
+    /*
+     * Getter and setter for player mode
+     */
     function getPlayerMode () {
         return playerMode;
     }
@@ -927,16 +921,9 @@ var Game = function () {
         view.headlineDisplay(getHeadlineElement());
     }
 
-    function resetResults () {
-        if (getPlayerMode() === 'Human') {
-            setPlayerSelection('');
-        }
-        resultModel.resetPlayerResults();
-        view.actualGameResultDisplay(getActualGameResultElement(), '');
-        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
-        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
-    }
-
+    /*
+     * Getter and setter for player selection
+     */
     function setPlayerSelection (selectKey) {
         playerSelection = selectKey;
         view.actualGameResultDisplay(getActualGameResultElement(), '');
@@ -947,15 +934,9 @@ var Game = function () {
         return playerSelection;
     }
 
-    function checkSelectionValue () {
-        if (getPlayerSelection() === '') {
-            view.displayNoSelection();
-            return true;
-        }
-
-        return false;
-    }
-
+    /*
+     * Method to get figure object by ramdom selection
+     */
     function computerSelectFigure () {
 
         var zz;
@@ -973,10 +954,34 @@ var Game = function () {
         return null;
     }
 
+    /*
+     * Method to get figure object by selection of human player
+     */
     function personSelectFigure () {
         return FigureFactory.createFigure(playerSelection);
     }
 
+    /*
+     * Helper method check if an item is selected
+     *
+     * shows dialog when no item is selected
+     */
+    function checkSelectionValue () {
+        if (getPlayerSelection() === '') {
+            view.displayNoSelection();
+            return true;
+        }
+
+        return false;
+    }
+
+    /*
+     * Handler for playing a game
+     *
+     * selects figures for players and determines result
+     *
+     * updates view elements
+     */
     function play () {
         var result;
 
@@ -1000,11 +1005,52 @@ var Game = function () {
         view.showSelectedDisplay(getSelectionDisplayElement(), playerOne.getName(), playerTwo.getName());
     }
 
+    /*
+     * Handler for reset of result
+     */
+    function resetResults () {
+        if (getPlayerMode() === 'Human') {
+            setPlayerSelection('');
+        }
+        resultModel.resetPlayerResults();
+        view.actualGameResultDisplay(getActualGameResultElement(), '');
+        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
+        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
+    }
+
+    /*
+     * init method for game
+     *
+     * 1. set node elements for display
+     * 2. creates and initializes game result model
+     * 3. creates game view object and setup view
+     */
+    function init (actualGameResult, headlineElement, resultElement, selectionElement, selectionDisplayElement) {
+
+        setActualGameResultElement(actualGameResult);
+        setHeadlineElement(headlineElement);
+        setResultElement(resultElement);
+        setSelectionElement(selectionElement);
+        setSelectionDisplayElement(selectionDisplayElement);
+
+        resultModel = new GameResultModel();
+        resultModel.init();
+
+        view = new GameView();
+        view.init(playerMode);
+        view.headlineDisplay(getHeadlineElement());
+        view.resultDisplay(getResultElement(), resultModel.getPlayerResults());
+        view.showSelectedDisplay(getSelectionDisplayElement(), '', '');
+        view.selectionDisplay(FigureType, getSelectionElement());
+    }
+
     return {
         init: init,
         play: play,
         resetResults: resetResults,
+        getPlayerMode: getPlayerMode,
         setPlayerMode: setPlayerMode,
+        getPlayerSelection: getPlayerSelection,
         setPlayerSelection: setPlayerSelection
     };
 };
